0446874fb88b4bb5c60b6e60725dcf52b2ce39bf
MaltheBisbo <30654168+MaltheBisbo@users.noreply.github.com>
2017-10-31 10:51:28 +0100

    Finished performance curves

--- krrThomas/performanceCurve.py
+++ krrThomas/performanceCurve.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+### BASECASE ###
 data = np.loadtxt('grendel/basecase_data/all_performance_base.txt', delimiter='\t')
 data = data.reshape((200,3))
 data = data[~np.isnan(data[:,0])]
@@ -9,21 +10,43 @@ Niter = data[:,0]
 Nfev = data[:,1]
 Ebest = data[:,2]
 
-Niter_values, Niter_base = np.histogram(Niter, bins=np.arange(201))
-Nfev_values, Nfev_base = np.histogram(Nfev, 200)
+Niter_values, Niter_base = np.histogram(Niter, 1000)
+Nfev_values, Nfev_base = np.histogram(Nfev, 1000)
 
 Niter_cum = np.cumsum(Niter_values)
 Nfev_cum = np.cumsum(Nfev_values)
 
+### MLenhanced ###
+dataML = np.loadtxt('grendel/MLenhanced_data/all_performance_MLenhanced.txt', delimiter='\t')
+
+NiterML = dataML[:,0]
+NfevML = dataML[:,1]
+EbestML = dataML[:,2]
+
+NiterML_values, NiterML_base = np.histogram(NiterML, 1000)
+NfevML_values, NfevML_base = np.histogram(NfevML, 1000)
+
+NiterML_cum = np.cumsum(NiterML_values)
+NfevML_cum = np.cumsum(NfevML_values)
+
 plt.figure(1)
-plt.title('Search performance - iterations (without ML)')
-plt.plot(np.arange(200)+0.5, Niter_cum/200)
+plt.title('Search performance - Search iterations (without ML)')
+plt.plot(Niter_base[:-1], Niter_cum/200, label='Without ML')
+plt.plot(NiterML_base[:-1], NiterML_cum/200, label='With ML')
+plt.legend()
 plt.xlabel('# search iterations until global minimum')
 plt.ylabel('Fraction of searches finging the global minimum')
 
 plt.figure(2)
-plt.title('Search performance - function evaluations (without ML)')
-plt.plot(Nfev_base[:-1], Nfev_cum)
+plt.title('Search performance - Function evaluations (without ML)')
+plt.plot(Nfev_base[:-1], Nfev_cum, label='Without ML')
+plt.plot(NfevML_base[:-1], NfevML_cum, label='With ML')
+plt.legend()
 plt.xlabel('# function evaluations until global minimum')
 plt.ylabel('Fraction of searches finging the global minimum')
 plt.show()
+
+
+
+
+
