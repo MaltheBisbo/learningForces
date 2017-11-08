41dfc4516d14fa36413ef99020f45f678c109785
MaltheBisbo <30654168+MaltheBisbo@users.noreply.github.com>
2017-11-08 20:59:50 +0100

    update

--- krrThomas/performanceCurve.py
+++ krrThomas/performanceCurve.py
@@ -29,7 +29,7 @@ NfevML_values, NfevML_base = np.histogram(NfevML, 1000)
 NiterML_cum = np.cumsum(NiterML_values)
 NfevML_cum = np.cumsum(NfevML_values)
 
-### MLenhanced - single target relax ###
+### MLenhanced - single target relax (Saving relaxed + unrelaxed)###
 dataML5 = np.loadtxt('grendel/dataN19_ML_singleTargetRelax6/all_performance_MLenhanced.txt', delimiter='\t')
 dataML5 = dataML5.reshape((200,7))
 dataML5 = dataML5[~np.isnan(dataML5[:,0])]
@@ -44,11 +44,27 @@ NfevML5_values, NfevML5_base = np.histogram(NfevML5, 1000)
 NiterML5_cum = np.cumsum(NiterML5_values)
 NfevML5_cum = np.cumsum(NfevML5_values)
 
+### MLenhanced - single target relax (Saving only relaxed)###
+dataML6 = np.loadtxt('grendel/dataN19_ML_singleTargetRelax7/all_performance_MLenhanced.txt', delimiter='\t')
+dataML6 = dataML6.reshape((200,7))
+dataML6 = dataML6[~np.isnan(dataML6[:,0])]
+
+NiterML6 = dataML6[:,0]
+NfevML6 = dataML6[:,1]
+EbestML6 = dataML6[:,2]
+
+NiterML6_values, NiterML6_base = np.histogram(NiterML6, 1000)
+NfevML6_values, NfevML6_base = np.histogram(NfevML6, 1000)
+
+NiterML6_cum = np.cumsum(NiterML6_values)
+NfevML6_cum = np.cumsum(NfevML6_values)
+
 plt.figure(1)
 plt.title('Search performance - Search iterations')
 plt.plot(Niter_base[:-1], Niter_cum/200, label='Without ML')
 plt.plot(NiterML_base[:-1], NiterML_cum/200, label='With ML')
 plt.plot(NiterML5_base[:-1], NiterML5_cum/200, label='With ML2')
+plt.plot(NiterML6_base[:-1], NiterML6_cum/200, label='With ML3')
 plt.legend()
 plt.xlabel('# search iterations')
 plt.ylabel('Succes rate')
@@ -58,6 +74,7 @@ plt.title('Search performance - Function evaluations')
 plt.plot(Nfev_base[:-1], Nfev_cum/200, label='Without ML')
 plt.plot(NfevML_base[:-1], NfevML_cum/200, label='With ML')
 plt.plot(NfevML5_base[:-1], NfevML5_cum/200, label='With ML2')
+plt.plot(NfevML6_base[:-1], NfevML6_cum/200, label='With ML3')
 plt.legend()
 plt.xlabel('# function evaluations')
 plt.ylabel('Succes rate')
--- krrThomas/testMinimizerTermination.py
+++ krrThomas/testMinimizerTermination.py
@@ -1,28 +1,43 @@
 import numpy as np
 from scipy.optimize import minimize
-from doubleLJ import doubleLJ
+from doubleLJ import doubleLJ_energy, doubleLJ_gradient
 import matplotlib.pyplot as plt
 
-def callback(x_cur):
+def Efun(X):
+    params = (1.5, 1, np.sqrt(0.02))
+    return doubleLJ_energy(X, params[0], params[1], params[2])
+
+def gradfun(X):
+    params = (1.5, 1, np.sqrt(0.02))
+    return doubleLJ_gradient(X, params[0], params[1], params[2])
+
+def callback(x_cur, obj):
     global Xtraj
     Xtraj.append(x_cur)
+    print(obj.fk)
 
 def localMinimizer(X):
     global Xtraj
     Xtraj = []
-    res = minimize(doubleLJ, X, method="BFGS", tol=1e-5, callback=callback)
+    res = minimize(Efun, X, method="BFGS",jac=gradfun, tol=1e-5, callback=callback)
     Xtraj = np.array(Xtraj)
     return res, Xtraj
+
 
 x = np.array([0, 0,
               0, 1.2,
               0.9, 0.2,
               1.3, 1.1])
 
-res, xrel = localMinimizer(x)
+res, Xtraj = localMinimizer(x)
+xrel = Xtraj[-1]
+Etraj = np.array([Efun(x) for x in Xtraj])
 
-
+plt.figure(1)
 plt.scatter(x[0::2], x[1::2], color='r')
-plt.scatter(xrel[-1][0::2], xrel[-1][1::2], color='b')
+plt.scatter(xrel[0::2], xrel[1::2], color='b')
 plt.gca().set_aspect('equal', adjustable='box')
+
+plt.figure(2)
+plt.plot(np.arange(len(Etraj)), Etraj)
 plt.show()
