angular gradient almost done, still a bug

krrThomas/angular_fingerprintFeature_m.py
@@ -4,6 +4,7 @@ import numpy as np
 from math import erf
 from itertools import product
 from scipy.spatial.distance import cdist
+import pdb
 
 try:
     cwd = sys.argv[1]
@@ -403,7 +404,7 @@ class Angular_Fingerprint(object):
         # Initialize 3body bondtype dictionary
         for bondtype in self.bondtypes_3body:
             feature_grad[1][bondtype] = np.zeros((self.Nbins2, n_atoms*self.dim))
-            
+
         # Calculate angular features
         for j in range(n_atoms):
             for n in range(len(nb_deltaRs_ang[j])):
@@ -467,17 +468,19 @@ class Angular_Fingerprint(object):
                         fc_jn_grad = self.__f_cutoff_grad(deltaR_n, self.gamma, self.Rc2)
                         fc_jm_grad = self.__f_cutoff_grad(deltaR_m, self.gamma, self.Rc2)
 
-                        dx_jn = nb_distVec[j][n]
-                        dx_jm = nb_distVec[j][m]
-                        index_jn = nb_index[j][n]
-                        index_jm = nb_index[j][m]
+                        dx_jn = nb_distVec_ang[j][n]
+                        dx_jm = nb_distVec_ang[j][m]
+                        index_jn = nb_index_ang[j][n]
+                        index_jm = nb_index_ang[j][m]
 
-                        a = -1/np.sqrt(1 - cos_angle**2)
-                        angle_j_grad = a * (-(dx_jn + dx_jm)/(deltaR_n*deltaR_m) + cos_angle*(dx_jn/deltaR_n**2 + dx_jm/deltaR_m**2))
-                        angle_n_grad = a * (dx_jm/(deltaR_n*deltaR_m) - cos_angle*dx_jn/deltaR_n**2)
-                        angle_m_grad = a * (dx_jn/(deltaR_n*deltaR_m) - cos_angle*dx_jm/deltaR_m**2)
-                        
-                            
+                        if not (angle == 0 or angle == np.pi):
+                            a = -1/np.sqrt(1 - cos_angle**2)
+                            angle_n_grad = a * (dx_jm/(deltaR_n*deltaR_m) - cos_angle*dx_jn/deltaR_n**2)
+                            angle_m_grad = a * (dx_jn/(deltaR_n*deltaR_m) - cos_angle*dx_jm/deltaR_m**2)
+                            angle_j_grad = -(angle_n_grad + angle_m_grad)
+                        else:
+                            angle_j_grad, angle_n_grad, angle_m_grad = (0,0,0)
+
                         # Define the index range of the gradient that belongs to each atom
                         index_range_j = np.arange(self.dim*index_jn[0], self.dim*index_jn[0]+self.dim)
                         index_range_n = np.arange(self.dim*index_jn[1], self.dim*index_jn[1]+self.dim)
@@ -551,3 +554,45 @@ class Angular_Fingerprint(object):
             return gamma*(gamma+1)/Rc * ((r/Rc)**gamma - (r/Rc)**(gamma-1))
         else:
             return 0
+        
+    def angle2_grad(self, RijVec, RikVec):
+        Rij = np.linalg.norm(RijVec)
+        Rik = np.linalg.norm(RikVec)
+        
+        a = RijVec/Rij - RikVec/Rik
+        b = RijVec/Rij + RikVec/Rik
+        A = np.linalg.norm(a)
+        B = np.linalg.norm(b)
+        D = A/B
+        
+        RijMat = np.dot(RijVec[:,np.newaxis], RijVec[:,np.newaxis].T)
+        RikMat = np.dot(RikVec[:,np.newaxis], RikVec[:,np.newaxis].T)
+
+        a_grad_j = -1/Rij**3 * RijMat + 1/Rij * np.identity(3)
+        b_grad_j = a_grad_j
+        
+        a_sum_j = np.sum(a*a_grad_j, axis=1)
+        b_sum_j = np.sum(b*b_grad_j, axis=1)
+        
+        grad_j = 2/(1+D**2) * (1/(A*B) * a_sum_j - A/(B**3) * b_sum_j)
+        
+        
+        
+        a_grad_k = 1/Rik**3 * RikMat - 1/Rik * np.identity(3)
+        b_grad_k = -a_grad_k
+        
+        a_sum_k = np.sum(a*a_grad_k, axis=1)
+        b_sum_k = np.sum(b*b_grad_k, axis=1)
+        
+        grad_k = 2/(1+D**2) * (1/(A*B) * a_sum_k - A/(B**3) * b_sum_k)
+        
+        
+        a_grad_i = -(a_grad_j + a_grad_k)
+        b_grad_i = -(b_grad_j + b_grad_k)
+        
+        a_sum_i = np.sum(a*a_grad_i, axis=1)
+        b_sum_i = np.sum(b*b_grad_i, axis=1)
+        
+        grad_i = 2/(1+D**2) * (1/(A*B) * a_sum_i - A/(B**3) * b_sum_i)
+        
+        return grad_i, grad_j, grad_k

krrThomas/grapheneRelax.py
@@ -48,7 +48,7 @@ def main():
     Nbins2 = 30
     sigma2 = 0.2
 
-    use_angular = False
+    use_angular = True
     gamma = 1
     eta = 50
 
@@ -73,30 +73,20 @@ def main():
 
     label = 'grapheneMLrelax/graphene1'
     calculator = krr_calculator(krr, label)
-<<<<<<< HEAD
-    
-    a = atoms_test_start[0]
-    a.set_calculator(calculator)
-    #dyn = SciPyFminBFGS(a, trajectory='grapheneMLrelax/graphene1.traj')
-    dyn = BFGS(a, trajectory='grapheneMLrelax/graphene1.traj')
-    dyn.run(fmax=0.1, n=200)
-=======
->>>>>>> d735d251e9d6370d2fc614064d979fa91b04cec5
 
     E_test_MLrelaxed = []
     atoms_test_MLrelaxed = []
     for i, a in enumerate(atoms_test_start):
         a.set_calculator(calculator)
-        #dyn = SciPyFminBFGS(a, trajectory='grapheneMLrelax/graphene1.traj')
-        dyn = BFGS(a, trajectory='grapheneMLrelax/graphene_test{}.traj'.format(i))
+        dyn = BFGS(a, trajectory='grapheneMLrelax/grapheneAng_test{}.traj'.format(i))
         dyn.run(fmax=0.1)
         atoms_test_MLrelaxed.append(a)
         E_test_MLrelaxed.append(krr.predict_energy(a))
 
-    write('grapheneMLrelax/graphene_MLrelaxed.traj', atoms_test_MLrelaxed)
+    write('grapheneMLrelax/grapheneAng_MLrelaxed.traj', atoms_test_MLrelaxed)
     energies = np.array([E_test_start, E_test_relaxed, E_test_MLrelaxed])
     energy_diff = np.array(E_test_relaxed) - np.array(E_test_MLrelaxed)
-    np.savetxt('grapheneMLrelax/graphene_MLrelaxed_E.txt', energies, delimiter='\t')
+    np.savetxt('grapheneMLrelax/grapheneAng_MLrelaxed_E.txt', energies, delimiter='\t')
     print('Energy differende:\n', energy_diff)
     
     

krrThomas/testAngleCalculation.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
+import pdb
+
 def angle1(v1,v2):
     """
     Returns angle with convention [0,pi]
@@ -15,7 +17,7 @@ def angle1(v1,v2):
     elif arg > 1:
         arg = 1.
     angle = np.arccos(arg)
-    return angle, arg
+    return angle
 
 def angle2(ri, rj, rk):
     RijVec = rj - ri
@@ -32,39 +34,117 @@ def angle2_grad(ri, rj, rk):
     RikVec = rk - ri
     Rij = np.linalg.norm(RijVec)
     Rik = np.linalg.norm(RikVec)
-    A = np.linalg.norm(RijVec/Rij - RikVec/Rik)
-    B = np.linalg.norm(RijVec/Rij + RikVec/Rik)
+    
+    a = RijVec/Rij - RikVec/Rik
+    b = RijVec/Rij + RikVec/Rik
+    A = np.linalg.norm(a)
+    B = np.linalg.norm(b)
     D = A/B
+
     RijMat = np.dot(RijVec[:,np.newaxis], RijVec[:,np.newaxis].T)
     RikMat = np.dot(RikVec[:,np.newaxis], RikVec[:,np.newaxis].T)
 
-    a = RijVec/Rij - RikVec/Rik
-    b = RijVec/Rij + RikVec/Rik
-    
-    a_grad_j = np.sum((-1/Rij**3 * RijMat + 1/Rij * np.identity(3)), axis=1)
+    a_grad_j = -1/Rij**3 * RijMat + 1/Rij * np.identity(3)
     b_grad_j = a_grad_j
-    #b_grad_j = np.sum((1/Rij**3 * RijMat - 1/Rij * np.identity(3)), axis=1)
 
     a_sum_j = np.sum(a*a_grad_j, axis=1)
     b_sum_j = np.sum(b*b_grad_j, axis=1)
     
     grad_j = 2/(1+D**2) * (1/(A*B) * a_sum_j - A/(B**3) * b_sum_j)
 
-    a_grad_k = np.sum((1/Rik**3 * RikMat - 1/Rik * np.identity(3)), axis=1)
-    b_grad_k = -a_grad_j
+
+
+    a_grad_k = 1/Rik**3 * RikMat - 1/Rik * np.identity(3)
+    b_grad_k = -a_grad_k
 
     a_sum_k = np.sum(a*a_grad_k, axis=1)
     b_sum_k = np.sum(b*b_grad_k, axis=1)
+    
+    grad_k = 2/(1+D**2) * (1/(A*B) * a_sum_k - A/(B**3) * b_sum_k)
 
 
+    a_grad_i = -(a_grad_j + a_grad_k)
+    b_grad_i = -(b_grad_j + b_grad_k)
+
+    a_sum_i = np.sum(a*a_grad_i, axis=1)
+    b_sum_i = np.sum(b*b_grad_i, axis=1)
+    
+    grad_i = 2/(1+D**2) * (1/(A*B) * a_sum_i - A/(B**3) * b_sum_i)
+    
+    return grad_i, grad_j, grad_k
+
+Natoms = 3
 L = 2
 d = 1
 dim = 3
-x = np.array([0.2*L, 0.7*L, d/2, 0.3*L, 0.2*L, d/2, 0.7*L, 0.9*L, 3*d/2])
-positions = x.reshape((-1,dim))
-ri = positions[0]
-rj = positions[1]
-rk = positions[2]
+theta = 1
+
+dx = 0.001
+Npoints = 199
+angle_old = np.zeros(Npoints)
+angle = np.zeros(Npoints)
+angle_grad = np.zeros((Npoints, dim))
+angle_grad_num = np.zeros((Npoints, dim))
+
+theta_fixed = 0.2
+theta_array = np.linspace(-2*np.pi, 2*np.pi, Npoints)
+for i, theta in enumerate(theta_array):
+    x = np.array([0, 0, 0,
+                  np.cos(theta), np.sin(theta), 0,
+                  np.cos(theta_fixed), np.sin(theta_fixed), 0])
+                  
+    pos = x.reshape((-1,dim))
+    ri = pos[0]
+    rj = pos[1]
+    rk = pos[2]
+    RijVec = rj - ri
+    RikVec = rk - ri
+
+    angle_old[i] = angle1(RijVec, RikVec)
+    angle[i] = angle2(ri, rj, rk)
+    angle_grad[i], _, _ = angle2_grad(ri, rj, rk)
+    
+    for j in range(dim):
+        x_pertub = np.zeros(Natoms*dim)
+        x_pertub[j] = dx
+
+        pos_down = np.reshape(x - x_pertub/2, (-1, dim))
+        pos_up = np.reshape(x + x_pertub/2, (-1, dim))
+
+        angle_down = angle2(pos_down[0], pos_down[1], pos_down[2])
+        angle_up = angle2(pos_up[0], pos_up[1], pos_up[2])
+        angle_grad_num[i,j] = (angle_up - angle_down)/dx
+        
+plt.figure(1)
+plt.plot(theta_array, angle_old)
+plt.plot(theta_array, angle, linestyle=':')
+
+plt.figure(2)
+plt.plot(theta_array, angle_grad_num)
+plt.plot(theta_array, angle_grad, linestyle=':')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
 RijVec = rj - ri
 RikVec = rk - ri
 Rij = np.linalg.norm(RijVec)
@@ -88,3 +168,4 @@ print('angle using angle1():', angle1(RijVec, RikVec))
 
 # Angle using function angle2()
 print('angle using angle2():', angle2(ri, rj, rk))
+"""

krrThomas/testFingerprintGradient.py
@@ -6,26 +6,27 @@ from angular_fingerprintFeature_m import Angular_Fingerprint
 from ase import Atoms
 from ase.visualize import view
 
-N = 2
+N = 3
 dim = 3
 
 L = 2
 d = 1
-pbc = [0,1,0]
+pbc = [1,0,1]
 
 """
 #x = np.array([0.2*L, 0.7*L, d/2])
 x = np.array([0.8*L, 0.2*L, d/2])
 positions = x.reshape((-1, dim))
-a = Atoms('H',
+atomtypes = ['H']
+a = Atoms(atomtypes,
           positions=positions,
           cell=[L,L,d],
           pbc=pbc)
-"""
+
 x = np.array([0.2*L, 0.5*L, d/2, 0.7*L, 0.5*L, d/2])
 #x = np.array([0.2*L, 0.7*L, d/2, 0.8*L, 0.2*L, d/2])
 positions = x.reshape((-1, dim))
-atomtypes=['H', 'H']
+atomtypes = ['H', 'H']
 a = Atoms(atomtypes,
           positions=positions,
           cell=[L,L,d],
@@ -38,7 +39,7 @@ a = Atoms(atomtypes,
           positions=positions,
           cell=[L,L,d],
           pbc=pbc)
-
+"""
 x = np.array([1, 0, 0, 2, 0, 0, 3, 0, 0, 1.5, 1, 0])
 positions = x.reshape((-1,dim))
 a = Atoms('H4',
@@ -66,7 +67,7 @@ Rc1 = 4
 binwidth1 = 0.1
 sigma1 = 0.2
 
-Rc2 = 2.1
+Rc2 = 3
 Nbins2 = 50
 sigma2 = 0.2
 
@@ -103,6 +104,6 @@ plt.plot(np.arange(len(fingerprint))*binwidth1+binwidth1/2, fingerprint)
 
 plt.figure(21)
 plt.plot(np.arange(len(fingerprint))*binwidth1, num_grad_fingerprint.T)
-plt.plot(np.arange(len(fingerprint))*binwidth1, grad_fingerprint, linestyle=':')
+plt.plot(np.arange(len(fingerprint))*binwidth1, grad_fingerprint, linestyle=':', color='k')
 plt.show()
 