b38d419ad1f8b6ba488bbc209e4c2636fe4bbfd2
MaltheBisbo <30654168+MaltheBisbo@users.noreply.github.com>
2018-03-13 08:22:49 +0100

    update

--- krrThomas/maternComparator.py
+++ krrThomas/maternComparator.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.spatial.distance import sqeuclidean
+from scipy.spatial.distance import euclidean
 from scipy.spatial.distance import cdist
 from scipy.special import binom
 from scipy.misc import factorial as fac
@@ -19,58 +19,80 @@ class maternComparator():
         if featureMat is not None:
             self.featureMat = featureMat
         v = self.n + 0.5
+        c = np.sqrt(2*v)/self.sigma
             
         d = cdist(self.featureMat, self.featureMat, metric='euclidean')
-        kernelMat = np.exp(-np.sqrt(2*v)/self.sigma * d) * self.__get_Pn(d)
+        kernelMat = np.exp(-c*d) * self.get_Pn(d)
         
         return kernelMat
     
-    def get_kernelVec(self, fnew, featureMat=None, split=False):
+    def get_kernelVec(self, f, featureMat=None, split=False):
         if featureMat is not None:
             self.featureMat = featureMat
         v = self.n + 0.5
         
-        d = cdist(fnew.reshape((1,len(fnew))), self.featureMat, metric='euclidean')
+        d = cdist(f.reshape((1,len(f))), self.featureMat, metric='euclidean')
         k1 = np.exp(-np.sqrt(2*v)/self.sigma * d)
-        k2 = self.__get_Pn(d)
+        k2 = self.get_Pn(d)
 
         if not split:
             return k1 * k2
         else:
             return k1, k2, d
 
-    def get_kernel(self, feature1, feature2, sigma=None, split=False):
+    def get_kernel(self, f1, f2, sigma=None, split=False):
         if sigma is None:
             sigma = self.sigma
         v = self.n + 0.5
-            
-        d = sqeuclidean(feature1, feature2)
-        k1 = np.exp(-np.sqrt(2*v)/self.sigma * d)
-        k2 = self.__get_Pn(d)
+        c = np.sqrt(2*v)/self.sigma
+        
+        d = euclidean(f1, f2)
+        k1 = np.exp(-c*d)
+        k2 = self.get_Pn(d)
 
         if not split:
             return k1 * k2
         else:
             return k1, k2, d
 
-    def get_kernel_Jac(self, fnew, featureMat):
+    def get_kernelVec_Jac(self, f, featureMat):
         """
-        Calculates tor jacobian of the similarity vector 'k' with respect
+        Calculates the jacobian of the similarity vector 'k' with respect
         to the feature vector 'f' of the new data-point.
         ie. calculates dk_df.
         Using the chain rule: dk_df = dk_dd*dd_df , where d is the distance measure
         """
-        k1Vec, k2Vec, dVec = self.get_kernelVec(fnew, featureMat, split=True)
-        v = self.n - 0.5
+        k1Vec, k2Vec, dVec = self.get_kernelVec(f, featureMat, split=True)
+        v = self.n + 0.5
         
         c = np.sqrt(2*v)/self.sigma
         dk1Vec_dd = -c*k1Vec
-        dk2Vec_dd = self.__get_Pn_deriv(dVec)
+        dk2Vec_dd = self.get_Pn_deriv(dVec)
         
         dkVec_dd = (k1Vec*dk2Vec_dd + k2Vec*dk1Vec_dd).reshape((len(k1Vec), 1))
-        dd_df = -2*(featureMat - fnew.reshape((1, len(fnew))))
+        dd_df = -(featureMat - f.reshape((1, len(f)))) / dVec.reshape((1,-1))
 
         dk_df = np.multiply(dkVec_dd, dd_df)
+        return dk_df
+
+    def get_kernel_Jac(self, f1, f2):
+        """
+        Calculates the jacobian of the similarity vector 'k' with respect
+        to the feature vector 'f' of the new data-point.
+        ie. calculates dk_df.
+        Using the chain rule: dk_df = dk_dd*dd_df , where d is the distance measure
+        """
+        k1, k2, d = self.get_kernel(f1, f2, split=True)
+        v = self.n + 0.5
+        c = np.sqrt(2*v)/self.sigma
+        
+        dk1_dd = -c*k1
+        dk2_dd = self.get_Pn_deriv(d)
+        
+        dk_dd = (k1*dk2_dd + k2*dk1_dd)
+        dd_df = -(f2 - f1)/d
+
+        dk_df = np.multiply(dk_dd, dd_df)
         return dk_df
     
     def get_kernel_Hess(self, f1, f2):
@@ -80,21 +102,21 @@ class maternComparator():
         ie. calculates: d^2/df1df2(kernel)
         """
         Nf = len(f1)
-        v = self.n - 0.5
+        v = self.n + 0.5
         c = np.sqrt(2*v)/self.sigma
         
         # kernel between the two features
         k1, k2, d = self.get_kernel(f1, f2, split=True)
 
         dk1_dd = -c*k1
-        dk2_dd = self.__get_Pn_deriv(d)
+        dk2_dd = self.get_Pn_deriv(d)
 
         d2k1_d2d = c**2*k1
-        d2k2_d2d = self.__get_Pn_2deriv(d)
+        d2k2_d2d = self.get_Pn_2deriv(d)
         
-        dd_df1 = -2*(f2-f1).reshape((Nf,1))
+        dd_df1 = -1/d*(f2-f1).reshape((Nf,1))
         dd_df2 = -dd_df1
-        d2d_df1df2 = -2*np.identity(Nf)
+        d2d_df1df2 = np.outer(f2-f1, f2-f1)/d**3 - 1/d*np.identity(Nf)
 
         grad_f1f2 = np.outer(dd_df1, dd_df2.T)
         Hess1 = k2 * (d2k1_d2d*grad_f1f2 + dk1_dd*d2d_df1df2)
@@ -104,7 +126,7 @@ class maternComparator():
         
         return Hess
 
-    def __get_Pn(self, d):
+    def get_Pn(self, d):
         n = self.n
         v = n + 0.5
         arg = 2*np.sqrt(2*v)/self.sigma * d
@@ -114,7 +136,7 @@ class maternComparator():
             Pn += fac(n+k)/fac(2*n) * binom(n,k) * arg**(n-k)
         return Pn
             
-    def __get_Pn_deriv(self, d):
+    def get_Pn_deriv(self, d):
         n = self.n
         v = n + 0.5
         c = np.sqrt(2*v)/self.sigma
@@ -126,7 +148,7 @@ class maternComparator():
         dPn_dd *= 2*c
         return dPn_dd
 
-    def __get_Pn_2deriv(self, d):
+    def get_Pn_2deriv(self, d):
         n = self.n
         v = n + 0.5
         c = np.sqrt(2*v)/self.sigma
--- krrThomas/plotMatern.py
+++ krrThomas/plotMatern.py
@@ -10,24 +10,53 @@ matern.set_args(**kwargs)
 f0 = np.array([0])
 f1 = np.array([1])
 
-Npoints = 1000
-x_array = np.linspace(0,2,Npoints)
+Npoints = 4000
+x_array = np.linspace(-2,2,Npoints)
+
 kernel = np.zeros(Npoints)
 kernel_Jac = np.zeros((Npoints,2))
 kernel_Hess = np.zeros((Npoints,2,2))
-x0=np.array([0,0])
+
+Pn = np.zeros(Npoints)
+Pn_deriv = np.zeros(Npoints)
+Pn_2deriv = np.zeros(Npoints)
+
+x0 = np.array([0,0])
 for i, x in enumerate(x_array):
-    x1=np.array([x,0])
-    kernel[i] = matern.get_kernel(x0,x1)
-    kernel_Jac[i] = matern.get_kernel_Jac(x0,x1.reshape((1,2)))
-    kernel_Hess[i] = matern.get_kernel_Hess(x0,x1)
-    
+    x1 = np.array([x,x])
+
+    kernel[i] = matern.get_kernel(x1,x0)
+    kernel_Jac[i] = matern.get_kernel_Jac(x1,x0)
+    kernel_Hess[i] = matern.get_kernel_Hess(x1,x0)
+
+    #d = x
+    #Pn[i] = matern.get_Pn(d)
+    #Pn_deriv[i] = matern.get_Pn_deriv(d)
+    #Pn_2deriv[i] = matern.get_Pn_2deriv(d)
+
+dx = x_array[1] = x_array[0]
 plt.figure()
 plt.plot(x_array, kernel)
 
 plt.figure()
 plt.plot(x_array, kernel_Jac)
+plt.plot(x_array, np.gradient(kernel, x_array), linestyle=':')
+plt.plot(x_array[:-1] + dx/2, (kernel[1:]-kernel[:-1])/dx)
 
 plt.figure()
-plt.plot(x_array, kernel_Hess.reshape((Npoints,-1)))
+plt.plot(x_array, kernel_Hess.reshape((Npoints,-1))[:,3])
+plt.plot(x_array, np.gradient(np.gradient(kernel, x_array), x_array), linestyle=':')
+
+"""
+plt.figure()
+plt.plot(x_array, Pn)
+
+plt.figure()
+plt.plot(x_array, Pn_deriv)
+plt.plot(x_array, np.gradient(Pn, x_array), linestyle=':')
+
+plt.figure()
+plt.plot(x_array, Pn_2deriv)
+plt.plot(x_array, np.gradient(np.gradient(Pn, x_array), x_array), linestyle=':')
+"""
 plt.show()
