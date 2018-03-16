6b8178fa50f184da4fdf5478a7a8eb8bbc3d7ee6
MaltheBisbo <30654168+MaltheBisbo@users.noreply.github.com>
2018-03-16 09:46:47 +0100

    update

--- krrThomas/test.py
+++ krrThomas/test.py
@@ -1,2 +1,19 @@
 import ase
+from ase.ga.startgenerator import StartGenerator
+
+from ase import Atoms
+from ase.visualize import view
+from ase.io import read, write
+
+atoms = read('graphene_data/graphene_all2.traj', index=':')
+a = atoms[0]
+atom_numbers = [1,1,1,1,1,1,1,1]
+closest_allowed_distances = 1.0
+
+startgenerator = StartGenerator(slab=a, atom_numbers=atom_numbers,
+                                closest_allowed_distances=closest_allowed_distances)
+
+a0 = startgenerator.get_new_candidate()
+view(a0)
+
 
