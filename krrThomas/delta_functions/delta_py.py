4ce7f2c354059f47380d1514549534b9d5a603fe
MaltheBisbo <30654168+MaltheBisbo@users.noreply.github.com>
2018-04-14 12:23:29 +0200

    delta function with cython

---
+++ krrThomas/delta_functions/delta_py.py
@@ -0,0 +1,36 @@
+import numpy as np
+from scipy.spatial.distance import euclidean
+
+class delta():
+
+    def __init__(self, cov_dist=1.0):
+        self.cov_dist = cov_dist
+
+    def energy(self, a):
+        rmin = 0.7 * self.cov_dist
+        radd = 1 - rmin
+        x = a.get_positions()
+        E = 0
+        for i, xi in enumerate(x):
+            for j, xj in enumerate(x):
+                if j > i:
+                    r = euclidean(xi, xj) + radd
+                    E += 1/r**12
+        return E
+
+    def forces(self, a):
+        rmin = 0.7 * self.cov_dist
+        radd = 1 - rmin
+        x = a.get_positions()
+        Natoms, dim = x.shape
+        dE = np.zeros((Natoms, dim))
+        for i, xi in enumerate(x):
+            for j, xj in enumerate(x):
+                r = euclidean(xi,xj)
+                r_scaled = r + radd
+                if j != i:
+                    rijVec = xi-xj
+
+                    dE[i] = 12*rijVec*(-1 / (r_scaled**13*r))
+
+        return - dE.reshape(-1)
