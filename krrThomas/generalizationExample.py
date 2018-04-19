917bd6622d2ace436b38f5a946456399899084c5
MaltheBisbo <30654168+MaltheBisbo@users.noreply.github.com>
2018-04-19 20:58:17 +0200

    edited generalisationExample

--- krrThomas/generalizationExample.py
+++ krrThomas/generalizationExample.py
@@ -36,8 +36,16 @@ def structure_list(x1_list, x2_list):
     a_list = [structure(x1, x2) for x1, x2 in zip(x1_list, x2_list)]
     return a_list
 
+class doubleLJ_delta():
+    def __init__(self, frac):
+        self.frac = frac
+
+    def energy(self, a):
+        return self.frac * E_doubleLJ(a)
+
 
 a0 = structure(0,2)
+
 
 # Set up featureCalculator
 Rc1 = 5
@@ -50,9 +58,10 @@ sigma2 = 0.2
 
 gamma = 1
 eta = 30
-use_angular = False
+use_angular = True
 
 featureCalculator = Angular_Fingerprint(a0, Rc1=Rc1, Rc2=Rc2, binwidth1=binwidth1, Nbins2=Nbins2, sigma1=sigma1, sigma2=sigma2, gamma=gamma, eta=eta, use_angular=use_angular)
+
 
 # Set up KRR-model
 comparator = gaussComparator()
@@ -63,26 +72,33 @@ krr = krr_class(comparator=comparator,
                 bias_fraction=0.7,
                 bias_std_add=1)
 
-train_coord = np.linspace(-0.5,0.5,10)
 
-
-x1_train = np.r_[train_coord, train_coord+2]
-x2_train = np.r_[train_coord+2, train_coord]
+# Generate training data
+train_coord = np.linspace(-0.2,0.2,3)
+x1_train = np.r_[train_coord]  # , train_coord+2]
+x2_train = np.r_[train_coord+2]  # , train_coord]
+#x1_train = np.r_[train_coord, train_coord+2]
+#x2_train = np.r_[-train_coord+2, -train_coord]
 
 a_train = structure_list(x1_train, x2_train)
 E_train = [E_doubleLJ(a) for a in a_train]
 
-GSkwargs = {'reg': [1e-7], 'sigma': np.logspace(-1, 2, 10)}
+
+# Train model
+GSkwargs = {'reg': [1e-7], 'sigma': [20]}
+#GSkwargs = {'reg': [1e-7], 'sigma': np.logspace(0,2,10)}
 MAE, params = krr.train(atoms_list=a_train, data_values=E_train, k=5, **GSkwargs)
 print(MAE, params)
 
+
+# Generate test data
 Npoints = 100
 test_coord = np.linspace(-0.5, 2.5, Npoints)
 x1_test = np.linspace(-0.5, 2.5, Npoints)
 x2_test = test_coord
 
+# Make grid of test data for contour plot
 X1, X2 = np.meshgrid(x1_test, x2_test)
-
 X1_flat = X1.reshape(-1)
 X2_flat = X2.reshape(-1)
 a_grid = structure_list(X1_flat, X2_flat)
@@ -91,13 +107,24 @@ E_grid = np.array([krr.predict_energy(a) for a in a_grid]).reshape((Npoints,Npoi
 E_grid_true = np.array([E_doubleLJ(a) for a in a_grid]).reshape((Npoints,Npoints))
 E_grid_true[E_grid_true > 1] = 0
 
-levels = np.linspace(-10,1,10)
+x1_path = test_coord
+x2_path = test_coord[::-1]
+a_path = structure_list(x1_path, x2_path)
+Epred_path = np.array([krr.predict_energy(a) for a in a_path])
+Etrue_path = np.array([E_doubleLJ(a) for a in a_path])
+
 plt.figure()
 plt.contourf(X1, X2, E_grid)
+plt.plot([-0.5,2.5], [2.5,-0.5], 'r:')
+plt.plot(x1_train, x2_train, 'kx')
 plt.colorbar()
 
 plt.figure()
 plt.contourf(X1, X2, E_grid_true)
 plt.colorbar()
+
+plt.figure()
+plt.plot(np.arange(Npoints), Etrue_path)
+plt.plot(np.arange(Npoints), Epred_path, 'k:')
 plt.show()
 
